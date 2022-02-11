d45a6d923dfc0f19d41c829b6345f648fcdcb846
kassiokennedy <kassio1206@hotmail.com>
2022-02-11 15:54:57 -0300

    teste

---
+++ demo01.js
@@ -0,0 +1,13 @@
+let nome = 'Super man'
+let idade = 100
+let poder = 'Força'
+// let pode ser alterada e const nao pode ser alterada
+nome = 'Batman'
+poder = 'rico'
+console.log(
+  `
+  nome: ${nome}
+  idade: ${idade}
+  poder: ${poder}
+   `
+)
---
+++ demo02.js
@@ -0,0 +1,12 @@
+let salario = 1000
+let meuSalario = '2999'
+let meuSalarioCorrigido = Number(meuSalario)
+console.log(meuSalarioCorrigido + salario)
+console.log(typeof salario, salario)
+console.log(typeof meuSalario, meuSalario)
+console.log(typeof meuSalarioCorrigido, meuSalarioCorrigido)
+console.log(isNaN(meuSalarioCorrigido))
+
+let minhaString = 'Olá, mundo!'
+let minhaOutraString = ' Olá, mundo doido!'
+console.log(minhaString + minhaOutraString)
